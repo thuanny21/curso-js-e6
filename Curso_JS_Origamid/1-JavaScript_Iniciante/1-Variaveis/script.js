36cd1e2391c705d8f9005f587ce1b7de12274643
Thuanny <thucatm@gmail.com>
2020-01-18 19:18:03 -0300

    add console.log

--- Curso_JS_Origamid/1-JavaScript_Iniciante/1-Variaveis/script.js
+++ Curso_JS_Origamid/1-JavaScript_Iniciante/1-Variaveis/script.js
@@ -1,15 +1,19 @@
 // Declarar uma variável com o seu nome
 var nome = 'Thuanny';
+console.log(nome);
 
 // Declarar uma variável com a sua idade
 var idade = 30;
+console.log(idade);
 
 // Declarar uma variável com a sua comida
 // favorita e não atribuir valor
 var comidaFavorita;
 
+
 // Atribuir valor a sua comida favorita
 var comidaFavorita = 'churrasco';
+console.log(comidaFavorita);
 
 // Declarar 5 variáveis diferentes sem valores
 var telefone,
