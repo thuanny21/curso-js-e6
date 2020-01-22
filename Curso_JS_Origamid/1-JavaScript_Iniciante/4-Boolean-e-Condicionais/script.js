23799d89b7c1df3af26575be0a2c4b0cb9f91951
Thuanny <thucatm@gmail.com>
2020-01-22 20:53:15 -0300

    atualização

--- Curso_JS_Origamid/1-JavaScript_Iniciante/4-Boolean-e-Condicionais/script.js
+++ Curso_JS_Origamid/1-JavaScript_Iniciante/4-Boolean-e-Condicionais/script.js
@@ -13,6 +13,7 @@ if(idade1 > idade2) {
 
 // Qual valor é retornado na seguinte expressão?
 var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(expressao);
 
 // Verifique se as seguintes variáveis são Truthy ou Falsy
 var nome = 'Andre';
@@ -21,9 +22,17 @@ var possuiDoutorado = false;
 var empregoFuturo;
 var dinheiroNaConta = 0;
 
+console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta);
+
 // Compare o total de habitantes do Brasil com China (valor em milhões)
 var brasil = 207;
 var china = 1340;
+
+if(brasil > china){
+  console.log('Brasil tem mais habitantes')
+} else {
+  console.log('Brasil tem menos habitantes')
+}
 
 // O que irá aparecer no console?
 if(('Gato' === 'gato') && (5 > 2)) {
